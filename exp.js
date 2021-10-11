a64e3891d238d18ba3d68637fc34e69ed2117165
Syed-Bitcot <68010192+Syed-Bitcot@users.noreply.github.com>
2021-10-11 17:01:59 +0530

    Added another port

--- exp.js
+++ exp.js
@@ -1,7 +1,7 @@
 const express = require('express')
-const express = require('express')
 const app = express()
 const port = 3000
+const another_port = 4000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
